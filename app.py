f5fa4ce83aee4241cb6ae399ab82dc82c94a6f29
Mandar <nadkarni.mandar96@gmail.com>
2025-11-22 21:19:20 -0500

    Make app cloud-ready and add Dockerfile

--- app.py
+++ app.py
@@ -123,4 +123,8 @@ with gr.Blocks(theme=gr.themes.Base(), fill_height=True) as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch()
+    port = int(os.environ.get("PORT", 8080))
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=port
+    )
